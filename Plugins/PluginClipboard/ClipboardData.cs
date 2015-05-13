71d271bd5037896eb6aab3c2110471960a3d3e39
Evgenii Vilkov <zzeneg@gmail.com>
2015-05-14 00:29:31 +0200

    Skip some DataFormats due to a bug in .NET
    Check
    https://social.msdn.microsoft.com/Forums/windows/en-US/ea562ff1-9558-425d-a30a-6a1809224edc/how-best-to-avoid-the-land-mines-planted-in-the-idataobject-returned-from-clipboardgetdataobject

--- Plugins/PluginClipboard/ClipboardData.cs
+++ Plugins/PluginClipboard/ClipboardData.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
@@ -12,6 +13,9 @@ namespace PluginClipboard
 
         private delegate TResult Func<in T, out TResult>(T arg);
 
+        /// <summary>
+        /// Serializers for different DataFormats
+        /// </summary>
         private readonly Dictionary<string, Func<DataObject, string>> _convertors = new Dictionary
             <string, Func<DataObject, string>>
         {
@@ -20,17 +24,25 @@ namespace PluginClipboard
             {DataFormats.Text, o => o.GetText()}
         };
 
+        /// <summary>
+        /// </summary>
+        /// <see cref="https://social.msdn.microsoft.com/Forums/windows/en-US/ea562ff1-9558-425d-a30a-6a1809224edc/how-best-to-avoid-the-land-mines-planted-in-the-idataobject-returned-from-clipboardgetdataobject"/>
+        private readonly string[] _buggyFormats = {DataFormats.EnhancedMetafile, DataFormats.MetafilePict, DataFormats.Palette};
+
         internal ClipboardData(IDataObject dataObject)
         {
             _dataObject = new DataObject();
             foreach (var format in dataObject.GetFormats())
             {
+                if (Array.Exists(_buggyFormats, s => s == format)) continue;
                 _dataObject.SetData(format, dataObject.GetData(format));
                 if (_convertors.ContainsKey(format))
                 {
                     _text = _convertors[format](_dataObject);
                 }
             }
+
+            if (_text == null) _text = "[DATA] " + DateTime.Now.ToString("T");
         }
 
         internal void SetToClipboard()
